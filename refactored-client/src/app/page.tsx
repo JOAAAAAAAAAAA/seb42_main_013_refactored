3beabc9c59c00d0d6071db1e8a0cd618f96ff597
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2023-08-14 14:09:38 +0900

    [design] Intro page CSS

--- refactored-client/src/app/page.tsx
+++ refactored-client/src/app/page.tsx
@@ -1,12 +1,26 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { BlueButton } from './components/Buttons'
+import Link from 'next/link'
+
 
 export default async function Home() {
-  await document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
+  // await document.documentElement.style.setProperty('--vh', `${window.innerHeight/100}px`);
 
   return (
-    <main className={styles.main}>
-
+    <main className={styles.container}>
+      <div className={styles.logo}>
+        {/* Image height auto 설정법 */}
+        <Image src="/images/logo1.png" alt="logo" width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto' }} />
+        <div className={styles.content}>당신을 위한 영양제 맞춤 서비스</div>
+      </div>
+      <div className={styles.joinOrLogin}>
+        <BlueButton text="시작하기" />
+        <div className={styles.loginWrap}>
+          <div>계정이 있으신가요?</div>
+          <Link href="/login" className={styles.loginButton}>로그인하기</Link>
+        </div>
+      </div>
     </main>
   )
 }
