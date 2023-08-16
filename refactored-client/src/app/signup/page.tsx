f07232def68cd969d9f27490aebb6dcf39e328f3
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2023-08-16 20:30:45 +0900

    [refeactor] styled-component to tailwind

--- refactored-client/src/app/signup/page.tsx
+++ refactored-client/src/app/signup/page.tsx
@@ -1,88 +1,101 @@
-import { useState, useEffect } from "react";
-import styled from "styled-components";
-import DataInput, { FakeInput, RealInput, SocialBtn } from "../components/DataInput";
-import { useForm } from "react-hook-form";
-import { CurrentBtn } from "../styles/Buttons";
-import { MypageConatiner } from "./MyPage";
-import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+"use client";
+// import DataInput, { FakeInput, RealInput, SocialBtn } from "../components/DataInput";
+// import { useForm } from "react-hook-form";
+// import { CurrentBtn } from "../styles/Buttons";
+// import axios from "axios";
+// import { Link, useNavigate } from "react-router-dom";
+// import { useSelector } from "react-redux";
+import Image from 'next/image'
 
-const Title=({href})=>{
-return (
-  <div style={{display:"flex"}}>
-   <img src={href} alt="아이콘" width="40px"/>
-   <div style={{fontSize:"40px", fontFamily:"NanumBarunGothicBold", letterSpacing:"1px", marginLeft:"10px"}}>I Pill U</div>
-  </div>
-)
-}
+// const Title=({href})=>{
+// return (
+//   <div style={{display:"flex"}}>
+//    <img src={href} alt="아이콘" width="40px"/>
+//    <div style={{fontSize:"40px", fontFamily:"NanumBarunGothicBold", letterSpacing:"1px", marginLeft:"10px"}}>I Pill U</div>
+//   </div>
+// )
+// }
 
-export const LoginBox = styled.form`
-  width: 100%;
-  >div{
-    padding-top: 8px;
-    padding-bottom: 8px;
-    margin-bottom: 16px;
-  }
-  >div:nth-last-child(1){
-    padding: 16px 0;
-  }
-  >div:last-child{
-    text-align: center;
-    color: var(--black-300);
-    span{
-      color: var(--blue-100)
-    }
-  }
-`;
-const Errorspan = styled.span`
-  color:red;  
-  font-size: 12px;
-`;
 
-const onClick=()=>{ window.location.href = '/login'};
+// export const LoginBox = styled.form`
+//   width: 100%;
+//   >div{
+//     padding-top: 8px;
+//     padding-bottom: 8px;
+//     margin-bottom: 16px;
+//   }
+//   >div:nth-last-child(1){
+//     padding: 16px 0;
+//   }
+//   >div:last-child{
+//     text-align: center;
+//     color: var(--black-300);
+//     span{
+//       color: var(--blue-100)
+//     }
+//   }
+// `;
+// const Errorspan = styled.span`
+//   color:red;  
+//   font-size: 12px;
+// `;
+
+// const onClick=()=>{ window.location.href = '/login'};
 
 function Signup() {
-  const { login } = useSelector(state => state.loginInfoReducer);
-  const navigate = useNavigate();
-  const [isFocus1, setIsFocus1] = useState(false);
-  const [isFocus2, setIsFocus2] = useState(false);
-  const [isFocus3, setIsFocus3] = useState(false);
+  // const { login } = useSelector(state => state.loginInfoReducer);
+  // const navigate = useNavigate();
+  // const [isFocus1, setIsFocus1] = useState(false);
+  // const [isFocus2, setIsFocus2] = useState(false);
+  // const [isFocus3, setIsFocus3] = useState(false);
 
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const [data, setData] = useState({ email: '', password: '', displayName: '' });
-  const onSubmit = async (data) => {
+  // const { register, handleSubmit, formState: { errors } } = useForm();
+  // const [data, setData] = useState({ email: '', password: '', displayName: '' });
+  // const onSubmit = async (data) => {
     // console.log(data);
-    await axios({
-      method: 'post',
-      // url: `/users`,
-      // url: `${process.env.REACT_APP_API_URL}/users`,
-      url: `http://ec2-3-35-105-108.ap-northeast-2.compute.amazonaws.com:8080/users`,
-      params: {},
-      data: data,
-    }, { withCredentials: true })
+  //   await axios({
+  //     method: 'post',
+  //     // url: `/users`,
+  //     // url: `${process.env.REACT_APP_API_URL}/users`,
+  //     url: `http://ec2-3-35-105-108.ap-northeast-2.compute.amazonaws.com:8080/users`,
+  //     params: {},
+  //     data: data,
+  //   }, { withCredentials: true })
 
-      .then((res) => {
-        alert('회원가입 완료')
-        window.location.href = '/signupDone'
-        // console.log(res)
-      })
-      .catch((err) => { console.log(err) })
-  };
-  const onError = (error) => {
-    console.log(error);
-  };
+  //     .then((res) => {
+  //       alert('회원가입 완료')
+  //       window.location.href = '/signupDone'
+  //       // console.log(res)
+  //     })
+  //     .catch((err) => { console.log(err) })
+  // };
+  // const onError = (error) => {
+  //   console.log(error);
+  // };
 
-  useEffect(() => {
-    if(login) {
-      navigate("/suggest");
-    }
-  }, [login])
+  // useEffect(() => {
+  //   if(login) {
+  //     navigate("/suggest");
+  //   }
+  // }, [login])
 
   return (
-    <MypageConatiner>
-      <Title href="/images/icon--ipu.png">I Pill U</Title>
-      <LoginBox onSubmit={handleSubmit(onSubmit, onError)}>
+    <div className="container
+    display: flex;
+    padding: 0 36px;
+    height: 100%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    gap: 32px;
+    h1 {
+      color: var(--black-100);
+      font-size: 28px;
+    }
+    ">
+      <div className="flex"><Image src="/images/icon--ipu.png" alt="I PILL U logo" width={40} height={40}/><p className="text-3xl font-semibold tracking-wide ml-2.5">I Pill U</p></div>
+      {/* <LoginBox onSubmit={handleSubmit(onSubmit, onError)}>
         <Errorspan>{errors.email && errors.email.message}</Errorspan>
         <FakeInput isFocus={isFocus1} isValid={errors.email?false:true}>
           <RealInput onFocus={() => setIsFocus1(true)} type="text" data={data} setData={setData} autoComplete="off" id="email" placeholder="이메일" name="email"
@@ -118,8 +131,8 @@ function Signup() {
         </FakeInput>
         <CurrentBtn>회원가입</CurrentBtn>
         <div>계정이 있으신가요?<Link to='/login' style={{color: "var(--blue-100)"}}>로그인</Link></div>
-      </LoginBox>
-    </MypageConatiner>
+      </LoginBox> */}
+    </div>
   )
 }
 
