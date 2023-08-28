4c5a0ae83831a7ed8133326de90a1853e4c433fa
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2023-08-28 14:13:00 +0900

    [refactor] useForm 사용으로 불필요한 코드 삭제

--- refactored-client/src/app/components/CustomInput.tsx
+++ refactored-client/src/app/components/CustomInput.tsx
@@ -1,171 +1,124 @@
+"use client";
 import { useState } from "react";
-import styled from "styled-components";
 
 
-export const FakeInput = styled.div`
-  display: flex;
-  flex: 1;
-  border : ${(props) => props.isFocus ?"1px solid var(--blue-100)" : "1px solid var(--black-400)"};
-  border-color: ${(props) => !props.isValid && "rgb(240, 86, 86)"};
-  box-shadow: ${(props) => ((props.isValid&&props.isFocus) && "0 0 2px 2px rgba(5, 145,255, .1)")};
-  border-radius: 5px;
-  /* 1줄 꽉채우게 */
-  padding: 0 8px;
-  position: relative;
-  transform: all .2s cubic-bezier(.645,.045,.355,1);
-  :hover{
-    background-color: #F7F9FA;
-  }
-  ::after{
-    position: absolute;
-    left:0;
-    bottom: -18px;
-    font-size: 12px;
-    color: rgb(240, 86, 86);
-    content: "${(props) => props.validationMessage}";
-  }
-`
 
-export const RealInput = styled.input`
-    border: none;
-    background-color: transparent;
-    box-shadow: none ;
-    font-size: 16px;
-    height: 32px;
-    width: 100%;
-    outline: none;
-    position:relative;
-    color: var(--black-100);
-  ::-webkit-outer-spin-button, ::-webkit-inner-spin-button{
-    -webkit-appearance: none;
-  }
-  ::-webkit-calendar-picker-indicator{
-    position:absolute;
-      /* 부모 relative - 자식 absolute */
-      /* 부모 relative 지정 안해주면 전체 화면 cover된다 */
-    left:0;
-    top:0;
-    width: 100%;
-    height: 100%;
-    color: transparent;
-    background: transparent;
-    cursor: pointer;
-  }
-  ::placeholder{
-    color: var(--black-400)
-  }
-  &[type='date']::-webkit-datetime-edit {
-    display: ${(props) => (!!props.value) ? "inline-block" : "none"};
-  }
-  &[type='date']::before{
-    position: absolute;
-    left: 0px;
-    color: var(--black-400);
-    content: "${(props) => props.placeholder}";
-    width: 100%;
-    display: ${(props) => !!props.value && "none"};
-  }
-  &[type='time']{
-    min-width: 100px;
-  }
-`
-export const DeleteBtn = styled.div`
-  display: flex;
-  align-items: center;
-  margin-left: 8px;
-  color: var(--black-200);
-  >button{
-    opacity : ${(props) => props.value ? "1" : "0"};
-    cursor: pointer;
-    background-color: transparent;
-    outline: 0;
-    border: 0;
-    width: 20px;
-    height: 20px;
-  }
-  svg{
-    color: var(--black-400);
-    width: 100%;
-    height: 100%;
-    fill: currentColor;
-  }
-`
+
+// export const RealInput = styled.input`
+//     border: none;
+//     background-color: transparent;
+//     box-shadow: none ;
+//     font-size: 16px;
+//     height: 32px;
+//     width: 100%;
+//     outline: none;
+//     position:relative;
+//     color: var(--black-100);
+//   ::-webkit-outer-spin-button, ::-webkit-inner-spin-button{
+//     -webkit-appearance: none;
+//   }
+//   ::-webkit-calendar-picker-indicator{
+//     position:absolute;
+//       /* 부모 relative - 자식 absolute */
+//       /* 부모 relative 지정 안해주면 전체 화면 cover된다 */
+//     left:0;
+//     top:0;
+//     width: 100%;
+//     height: 100%;
+//     color: transparent;
+//     background: transparent;
+//     cursor: pointer;
+//   }
+//   ::placeholder{
+//     color: var(--black-400)
+//   }
+//   &[type='date']::-webkit-datetime-edit {
+//     display: ${(props) => (!!props.value) ? "inline-block" : "none"};
+//   }
+//   &[type='date']::before{
+//     position: absolute;
+//     left: 0px;
+//     color: var(--black-400);
+//     content: "${(props) => props.placeholder}";
+//     width: 100%;
+//     display: ${(props) => !!props.value && "none"};
+//   }
+//   &[type='time']{
+//     min-width: 100px;
+//   }
+// `
+// export const DeleteBtn = styled.div`
+//   display: flex;
+//   align-items: center;
+//   margin-left: 8px;
+//   color: var(--black-200);
+//   >button{
+//     opacity : ${(props) => props.value ? "1" : "0"};
+//     cursor: pointer;
+//     background-color: transparent;
+//     outline: 0;
+//     border: 0;
+//     width: 20px;
+//     height: 20px;
+//   }
+//   svg{
+//     color: var(--black-400);
+//     width: 100%;
+//     height: 100%;
+//     fill: currentColor;
+//   }
+// `
 // const ValidityMsg = styled.div`
 //   color: rgb(240, 86, 86);
 //   font-size: 12px;
 //   opacity : ${(props) => props.isValid ? "0" : "1"};
 // `
 
+type CustomInputProps = {
+  type: string;
+  placeholder: string;
+  register:
+}
 
-function DataInput ({name, min, max, required, placeholder, data, setData, type}) {
-  const [isFocus, setIsFocus] = useState(false)
+
+function CustomInput ({type, placeholder, register}:CustomInputProps) {
+  // focus 대신 focus-within 사용하면 상태 props 전달 필요 없음
+  // const [isFocus, setIsFocus] = useState(false)
   const [isValid, setIsValid] = useState(true)
   const [validityState, setValidityState] = useState("")
   
-  const getValidtyState = (e) => {
-    if(!e.target.validity.valid){
-      let validity = ''
-      for(let key in e.target.validity){
-        if(e.target.validity[key]) validity = key
-      }
-      return validity
-    }
-  }
-  const validationMessage = (validityState) => {
-    let message = ''
-    switch(validityState) {
-      case "rangeOverflow" :
-        message = "전체 용량보다 적어야 합니다."
-      break;
-      case "valueMissing" :
-        message = "필수 입력 항목입니다."
-      break;
-      case "rangeUnderflow" :
-        message = "0 이상의 숫자를 입력해주세요."
-      break;
-      case "customError" :
-        message = "잔여 알 수 보다 많아야 합니다."
-      break;
-      case "badInput" :
-        message = "유효한 값을 입력해주세요."
-      break;
-      default:
-        message = ""
-    }
-    return message
-  }
-
-
-  const changeHandler = (e) => {
-    setData({...data,[name]:e.target.value})
-    // !isValid && setIsValid(e.target.checkValidity())
-    // 한번 blur 후에 유효성 체크 되도록
-    if(name==="totalCapacity" && Number(e.target.value)<Number(data.pillsLeft)){
-      e.target.setCustomValidity("lessThanCurrent")
-    } else {
-      e.target.setCustomValidity("")
-    }
-    !isValid && setIsValid(e.target.validity.valid)
-    !isValid && setValidityState(getValidtyState(e))//message 출력을 위한 state 설정
-  }
   const clear = () => {
     setData({...data,[name]:""})
   }
-  const blurHandler = (e) => {
-    setIsValid(e.target.validity.valid)
-    setValidityState(getValidtyState(e))
-    setIsFocus(false)
-  }
-  const messageStopper = (e) => {
-    //브라우저 Invalid 메세지 hide를 위해
-    e.preventDefault();
-    //custom message showing
-    setIsValid(false)
-    setValidityState(getValidtyState(e))
-  }
+
   return (
-      <FakeInput isFocus={isFocus} isValid={isValid} validationMessage={validationMessage(validityState)}>
-        <RealInput
+    <div>
+      <label
+        htmlFor={name}
+        className={`font-bold text-[--black-200] text-base after:content-[${required ? "*" : ""}]`}
+      >{name}</label>
+      <div className={`flex flex-1 border 
+        ${isValid ? "focus-within:border-2 focus-within:border-[--blue-100] focus-within:shadow-[0_0_2px_2px_rgba(5, 145,255, .1)] border-[--black-400]" :"border-[--red-100]"} 
+        rounded-[5px] py-2 relative 
+        hover:bg-[#F7F9FA] transition-colors 
+        after:absolute after:left-0 after:bottom-[-18px] after:font-xs after:text-rose-500 after:content-[${validityState}]`}
+      >
+        <input 
+        className="border-none bg-transparent shadow-none text-base h-8 w-full outline-none relative text-[--black-100]
+        placeholder:text-[--black-400]
+        "
+        required={required}
+        min={min}
+        max={max}
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        onChange={changeHandler}
+        onBlur={blurHandler}
+        onInvalid={messageStopper}
+        />
+        {/* <RealInput
           onFocus={()=>setIsFocus(true)}
           onBlur={blurHandler}
           type={type} 
@@ -177,14 +130,14 @@ function DataInput ({name, min, max, required, placeholder, data, setData, type}
           max={max}
           name={name}
           onInvalid={messageStopper}
-        />
+        /> */}
         {/* { name === "ingredientAmount" &&
           <DeleteBtn>mg</DeleteBtn>
         } */}
-        { name === "dose" &&
+        {/* { name === "dose" &&
           <DeleteBtn>알</DeleteBtn>
-        }
-        { type!=="date" &&
+        } */}
+        {/* { type!=="date" &&
         <>
           <DeleteBtn value={!!data[name]} >
             <button type="button" onClick={()=>clear()}>
@@ -194,9 +147,10 @@ function DataInput ({name, min, max, required, placeholder, data, setData, type}
             </button>
           </DeleteBtn>
         </>
-        }
-      </FakeInput>
+        } */}
+      </div>
+    </div>
   )
 }
 
-export default DataInput;
+export default CustomInput;
