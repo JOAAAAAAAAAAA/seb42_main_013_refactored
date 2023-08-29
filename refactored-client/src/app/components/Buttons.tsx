e2e6477e2a97c28d892acc1adecf89a45dea76e5
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2023-08-29 20:34:49 +0900

    [design] oauth 계정별 로그인 버튼

--- refactored-client/src/app/components/Buttons.tsx
+++ refactored-client/src/app/components/Buttons.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import styles from "./Buttons.module.css";
 import React, { ButtonHTMLAttributes, ReactNode } from "react";
 
@@ -9,9 +10,26 @@ interface BlueButtonProps {
   disabled?: ButtonHTMLAttributes<HTMLButtonElement>["disabled"];
 }
 
-export const BlueButton = ({children, onClick, type, disabled}:BlueButtonProps) => {
+export const BlueButton = ({ children, onClick, type, disabled }: BlueButtonProps) => {
   return (
     <button className={styles.blueButton} onClick={onClick} type={type} disabled={disabled}>
+      {children}
+    </button>
+  )
+}
+
+interface SignupButtonProps {
+  children?: ReactNode;
+  onClick?: () => void;
+  oauthProvider: 'facebook' | 'google' | 'kakao' | 'github';
+}
+
+export const SignupButton = ({ children, onClick, oauthProvider }: SignupButtonProps) => {
+  return (
+    //! 변수 사용시 []로 감싸야 함
+    <button className={styles[oauthProvider]} onClick={onClick}>
+      <Image src={`/svg/${oauthProvider}.svg`} alt={`${oauthProvider} icon`} width={18} height={18}/>
+      {oauthProvider} 계정으로 시작하기
       {children}
     </button>
   )
--- refactored-client/src/app/signup/page.tsx
+++ refactored-client/src/app/signup/page.tsx
@@ -9,6 +9,7 @@ import Image from 'next/image'
 import CustomInput from '../components/FormInput';
 import SignupForm from '../components/SignupForm';
 import Link from 'next/link';
+import { SignupButton } from '../components/Buttons';
 
 
 // const Title=({href})=>{
@@ -62,10 +63,16 @@ function Signup() {
   // }, [login])
 
   return (
-    <div className="container flex flex-col font-nanumGothic px-9 h-full items-center justify-center gap-8">
+    <div className="container flex flex-col w-full font-nanumGothic px-9 h-full items-center justify-center gap-8">
       <div className="flex items-center justify-center"><Image src="/images/icon--ipu.png" alt="I PILL U logo" width={40} height={40} /><p className="text-[40px] font-semibold tracking-wide ml-2.5">I Pill U</p></div>
       <SignupForm />
       <div className="flex w-full items-center tracking-wide justify-center text-center text-[--black-300]">계정이 있으신가요?<Link href="/login" className="text-[--blue-100] ml-0.5" >로그인</Link></div>
+      <div className="flex flex-col shrink-0 w-full gap-[4px]">
+      <SignupButton oauthProvider='google'/>
+      <SignupButton oauthProvider='github'/>
+      <SignupButton oauthProvider='facebook'/>
+      <SignupButton oauthProvider='kakao'/>
+      </div>
       {/* <div className="flex px-2	gap-6"><Image src="/images/btn_google_signin_light_normal_web@2x.png" alt="google icon" width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto' }} /></div> */}
     </div>
   )
