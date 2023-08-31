7a1f9d9bfaf6714eab5f4515ee150e56800a0e37
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2023-08-31 18:38:52 +0900

    [design] Header component

--- refactored-client/src/app/components/Header.tsx
+++ refactored-client/src/app/components/Header.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 function Header() {
 
   return (
-    <div className="bg-[--blue-100] w-full max-w-[428px] h-[48px] fixed top-0 flex justify-between items-center px-[20px]">
-      <Link href="/"><Image src="images/logo_header.png" alt="logo" width={120} className="mt-[2px]" /></Link>
+    <div className="bg-[--blue-100] w-full max-w-[428px] h-[48px] flex justify-center items-center px-[20px]">
+      <Link href="/"><Image src="/images/logo_header.png" alt="logo" width={120} height={34} className="mt-[2px]" /></Link>
     </div>
   )
 }
