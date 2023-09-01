09078daadc675f3940fd94a31fdec8dd42f8bfe1
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2023-09-01 15:28:25 +0900

    [fix] signinwithpop 은 가능

--- refactored-client/src/app/@login/signup/helper.ts
+++ refactored-client/src/app/@login/signup/helper.ts
@@ -3,12 +3,24 @@ import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context';
 import { auth, googleAuthProvider } from '../../../../firebase/firebaseApp';
 import { addUserToFirestore } from '../../../../firebase/userController';
 
+// export const handleGoogleSignIn = (router:AppRouterInstance) => {
+//   signInWithPopup(auth, googleAuthProvider)
+//   .then ((result) => {
+//     const user = result?.user;
+//     console.log("user",user)
+//     addUserToFirestore (user) ;
+//     router.push ("/");
+//   })
+//   .catch((error) => {
+//     throw new Error (error. message);
+//   });
+// } 
+
 export const handleGoogleSignIn = (router:AppRouterInstance) => {
   signInWithPopup(auth, googleAuthProvider)
   .then ((result) => {
     const user = result?.user;
     console.log("user",user)
-
     addUserToFirestore (user) ;
     router.push ("/");
   })
@@ -16,4 +28,3 @@ export const handleGoogleSignIn = (router:AppRouterInstance) => {
     throw new Error (error. message);
   });
 } 
-
