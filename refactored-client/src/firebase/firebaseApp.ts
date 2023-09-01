66754d8758cd2573ef3754559b628e709b80037e
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2023-09-02 01:18:59 +0900

    [fix] firebase app initialize code 수정

---
+++ refactored-client/src/firebase/firebaseApp.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { getApps, initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = getApps().length ?getApps()[0] :initializeApp(firebaseConfig);
+const googleAuthProvider = new GoogleAuthProvider();
+const auth = getAuth();
+const firestore = getFirestore(app);
+
+export { app, googleAuthProvider, auth, firestore };
