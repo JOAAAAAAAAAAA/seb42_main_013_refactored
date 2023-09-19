8300c4bb6ef4672373803960dd8fe9e7cf12a26a
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2023-09-19 21:31:36 +0900

    [comment] 헤더 다른 방법으로

--- refactored-client/src/app/@header/default.tsx
+++ refactored-client/src/app/@header/default.tsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 
 export default function HeaderDefault() {
   return (
-    <header className="bg-[--blue-100] w-full max-w-[428px] h-[48px] flex justify-center items-center px-[20px]">
+    <header className="flex h-[48px] w-full max-w-[428px] items-center justify-center bg-[--blue-100] px-[20px]">
       <Link href="/"><Image src="/images/logo_header.png" alt="logo" width={120} height={34} className="mt-[2px]" /></Link>
     </header>
   )
 }
 
+//parallel-routes 로 하면 페이지 전환 때마다 불필요한 렌더링이 발생해서 안하는게 좋을 것 같다.
