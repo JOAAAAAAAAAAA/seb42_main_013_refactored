dce5b75e26dbefe9aee15180c926383a44878829
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2023-10-04 21:16:03 +0900

    [feat] Multiple Actions Per Route

--- refactored-client/src/lib/data.ts
+++ refactored-client/src/lib/data.ts
@@ -9,63 +9,73 @@ import { redirect } from 'next/navigation'
 import { z } from 'zod'
 
 export const createData = async (prevFormState: Pill, formData: FormData) => {
-  console.log('prevFormState', prevFormState)
-  console.log('formData', formData)
-
 
   //https://github.com/remix-run/remix/discussions/1298
   switch (formData.get('type')) {
     case 'update_ingredients':
-      const ingredient = formData.get('ingredients') 
-      if(typeof ingredient === 'string' && !prevFormState.ingredients.includes(ingredient)){
+      const ingredient = formData.get('ingredients')
+      if (
+        typeof ingredient === 'string' &&
+        !prevFormState.ingredients.includes(ingredient)
+      ) {
         //중복값 입력 안되게
         return {
           ...prevFormState,
           ingredients: [...prevFormState.ingredients, ingredient],
         }
-      }else{
+      } else {
         return prevFormState
       }
     case 'update_takingTime':
       const time = formData.get('takingTime')
-      if(typeof time === 'string' && !prevFormState.takingTime.includes(time)){
+      if (
+        typeof time === 'string' &&
+        !prevFormState.takingTime.includes(time)
+      ) {
         //중복값 입력 안되게
         return {
           ...prevFormState,
           takingTime: [...prevFormState.takingTime, time],
         }
-      }else{
+      } else {
         return prevFormState
       }
     case 'deleteChip':
       const fieldsetName = formData.get('fieldsetName')
       const value = formData.get('value')
-      if(typeof fieldsetName === 'string'){
-        const filtered = prevFormState.takingTime.filter((ele)=> ele !== value)
+      if (fieldsetName === 'takingTime' || fieldsetName === 'ingredients') {
+        const filtered = prevFormState[fieldsetName].filter(
+          (ele) => ele !== value,
+        )
         // revalidatePath('/create')
+        // useFormState랑 엮어놔서 revalidate 안해도 해당 컴포넌트 리렌더링됨
         return {
           ...prevFormState,
           [fieldsetName]: [...filtered],
         }
-      }else return prevFormState
+      } else return prevFormState
     case 'create':
-      const schema = z.object({
-        supplementName: z.string(),
-        ingredients: z.array(z.string().nullable()),
-        productType: z.string(),
-        formulation: z.string(),
-        expirationDate: z.string().nullable(),
-        startDate: z.string().default(new Date().toISOString().slice(0, 10)),
-        endDate: z.string(),
-        takingTime: z.array(z.string().nullable()),
-        pillsLeft: z.coerce.number(),
-        totalCapacity: z.coerce.number(),
-        servingSize: z.coerce.number(),
-      })
+      // const schema = z.object({
+      //   supplementName: z.string(),
+      //   ingredients: z.array(z.string().nullable()),
+      //   productType: z.string(),
+      //   formulation: z.string(),
+      //   expirationDate: z.string().nullable(),
+      //   startDate: z.string().default(new Date().toISOString().slice(0, 10)),
+      //   endDate: z.string(),
+      //   takingTime: z.array(z.string().nullable()),
+      //   pillsLeft: z.coerce.number(),
+      //   totalCapacity: z.coerce.number(),
+      //   servingSize: z.coerce.number(),
+      // })
       const year = formData.get('expirationDate_year') ?? ''
       const month = formData.get('expirationDate_month') ?? ''
       const day = formData.get('expirationDate_day') ?? ''
-      if(typeof year === 'string' && typeof month === 'string' && typeof day === 'string'){
+      if (
+        typeof year === 'string' &&
+        typeof month === 'string' &&
+        typeof day === 'string'
+      ) {
         const expirationDate = `${year}-${month}-${day}`
         formData.set('expirationDate', expirationDate)
       }
@@ -73,49 +83,21 @@ export const createData = async (prevFormState: Pill, formData: FormData) => {
       //타입 이슈 해결
       const prev = Object.entries(prevFormState)
       const updated = prev.map(([key, prevValue]) => {
-        const newValue = (key==="takingTime" || key==="ingredients") ?formData.getAll(key): formData.get(key)
+        const newValue =
+          key === 'takingTime' || key === 'ingredients'
+            ? formData.getAll(key)
+            : formData.get(key)
         //takingTime, ingredients은 배열이므로 getAll로 모두 뽑는다
-        return [
-          key,
-          prevValue !== newValue ? newValue : prevValue,
-        ]
+        return [key, prevValue !== newValue ? newValue : prevValue]
       })
       const updatedFormState = Object.fromEntries(updated)
+      console.log('updatedFormState', updatedFormState)
 
-      try {
-        const parsedData = schema.safeParse(updatedFormState)
-        console.log('parsedData', parsedData)
-        //해당 url방문시에 revalidate 되는 거라 먼저 선언해도 괜찮
-        revalidatePath('/create')
-        return updatedFormState
-      } catch (e) {
-        console.log('error', e)
-        return prevFormState
-      }
-
+      const parsedData = schema.safeParse(updatedFormState)
+      console.log('parsedData', parsedData)
+      //해당 url방문시에 revalidate 되는 거라 먼저 선언해도 괜찮
+      return updatedFormState
     default:
       return prevFormState
   }
-
-  // finally{
-  //   console.log('finally')
-  //   revalidatePath('./create')
-  //   redirect('./create') //  'dashboard/create'로 입력하면 /dashboard/dashboard/create로 된다. 상대경로라 그런듯
-  // }
 }
-
-export const deletechip = async (prevFormState: any, formData: FormData) => {
-
-}
-
-
-export const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault()
-  const form = e.currentTarget
-  const formData = new FormData(form)
-  console.log(formData)
-  await formAction(formData)
-  revalidatePath('/dashboard/create')
-  redirect('/dashboard/create')
-}
-
