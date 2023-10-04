c22af439182610f3e147af27d97ffc700f448dcd
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2023-10-04 21:31:40 +0900

    [feat] from reducer to useFormState

--- refactored-client/src/app/dashboard/create/CreateModal.tsx
+++ refactored-client/src/app/dashboard/create/CreateModal.tsx
@@ -1,51 +1,51 @@
 "use client"
 
-import { BlueButton } from "@/app/components/Buttons";
-import { Backdrop, Button, Modal, TextField } from "@mui/material";
-import { useRouter } from "next/navigation";
-import { useRef, Dispatch } from "react";
-import { ModalAction, ModalState } from "./page"
+import { Backdrop, Button, TextField } from "@mui/material";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useRef } from "react";
 
 
 export default function CreateModal({
-  fieldName,
-  dispatch,
+  addChip,
 }: {
-  fieldName: string;
-  dispatch: Dispatch<ModalAction>;
+  addChip: (e: React.FormEvent<HTMLFormElement>) => Promise<void>;
 }) {
+  const searchParams = useSearchParams()
+  const fieldName = searchParams.get('fieldset') ?? ""
   const router = useRouter()
   const inputRef = useRef<HTMLInputElement>(null);
+  const pathname = usePathname()
+
   return (
     <Backdrop
       className="z-10"
-      open={true} onClick={(router.back)}>
-      <div
-        onClick={(e) => e.stopPropagation()}
-        className="relative m-auto flex max-w-[300px] flex-col gap-[4px] rounded-[5px] bg-white px-[20px] py-[16px]">
-        <TextField
-          className="cleanInput !min-w-[150px]"
-          sx={{
-            '& .MuiFormLabel-asterisk,.MuiInputLabel-asterisk':{
-              display: "none"
-            },
-          }}
-          label={fieldName === "ingredients" ? "성분명" : "복용 시간"}
-          type={fieldName === "ingredients" ? "text" : "time"}
-          id={fieldName}
-          required
-          name={fieldName}
-          inputRef={inputRef}
-        />
-        <Button
-          variant="contained"
-          onClick={() => {
-            fieldName === "ingredients"
-              ? dispatch({ type: "AddIngredients", ingredient: inputRef.current?.value })
-              : dispatch({ type: "AddTakingTime", time: inputRef.current?.value })
-            router.back()
-          }}
-        >추가</Button>
+      open={true} onClick={()=>router.push(pathname)}>
+      <div onClick={(e) => e.stopPropagation()}>
+        <form
+          // action={formAction} 
+          //! revalidate & routerback처리 위해 custom onsubmit
+          onSubmit={(e) => addChip(e)}
+          className="relative m-auto flex max-w-[300px] flex-col gap-[4px] rounded-[5px] bg-white px-[20px] py-[16px]">
+          <TextField
+            className="cleanInput !min-w-[150px]"
+            sx={{
+              '& .MuiFormLabel-asterisk,.MuiInputLabel-asterisk': {
+                display: "none"
+              },
+            }}
+            label={fieldName === "ingredients" ? "성분명" : "복용 시간"}
+            type={fieldName === "ingredients" ? "text" : "time"}
+            id={fieldName}
+            required
+            name={fieldName}
+            inputRef={inputRef}
+          />
+          <input name="type" type="hidden" defaultValue={`update_${fieldName}`} />
+          <Button
+            type="submit"
+            variant="contained"
+          >추가</Button>
+        </form>
       </div>
     </Backdrop>
   )
