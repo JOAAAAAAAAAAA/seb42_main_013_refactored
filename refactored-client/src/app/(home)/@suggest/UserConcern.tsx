ad010a5d1af5238ab0b141c02b61aaf97fa4fe03
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2023-10-13 10:19:45 +0900

    [fix] client 컴포넌트로 전환 hydrate

--- refactored-client/src/app/(home)/@suggest/UserConcern.tsx
+++ refactored-client/src/app/(home)/@suggest/UserConcern.tsx
@@ -1,27 +1,30 @@
+"use client"
+
 import { getUser } from "@/lib/user"
+import  Card  from "@mui/material/Card"
 import  Button  from "@mui/material/Button"
 import { cookies } from "next/headers"
 import Image from "next/image"
 import Link from "next/link"
+import { useContext } from "react"
+import { AuthContext } from "@/context/AuthProvider"
 
 
-export default async function UserConcern() {
+export default function UserConcern() {
 
-  const { default : Card } = await import('@mui/material/Card')
-
-  const sessionCookie = cookies().get('session')?.value || ""
-  const user = await getUser(sessionCookie)
-
+  const { authUser } = useContext(AuthContext);
+  console.log('authUser', authUser)
+  
   return (
     <>
-      <div className="pl-[4px] text-[14px]"> 환영합니다, <span>{user?.displayName}</span>님✨</div>
+      <div className="pl-[4px] text-[14px]"> 환영합니다, <span>{authUser?.displayName}</span>님✨</div>
       <Card variant='outlined' className="p-[--gap-md]">
-        {user?.concerns
+        {authUser?.concerns
           ? (
             <div className="flex flex-col">
-              <div className="mb-[--gap-sm]" ><span className="font-bold text-[--blue-100]">{user?.displayName}</span>님을 위한 영양제 추천</div>
+              <div className="mb-[--gap-sm]" ><span className="font-bold text-[--blue-100]">{authUser?.displayName}</span>님을 위한 영양제 추천</div>
               <div className="relative flex h-[100px] w-full items-center justify-around">
-                {user.concerns.map((ele: string, idx: number) => (
+                {authUser.concerns.map((ele: string, idx: number) => (
                     <div className="text-center" key={idx}>
                       <Image src={`/images/icon-pill${idx}.png`} width={50} height={50} alt="supplement-icon" />
                       <span className="text-xs">{ele}</span>
