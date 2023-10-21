703866f13e0f1fd36dc63a63f0c9f3eff5f2af9f
JOAAAAAAAAAAA <blueseablueskyblueme@gmail.com>
2023-10-21 16:09:34 +0900

    [test] verecl test

--- src/app/(home)/@suggest/test/page.tsx
+++ src/app/(home)/@suggest/test/page.tsx
@@ -2,49 +2,49 @@ import Image from "next/image"
 import { Item } from "@/types"
 import { getPlaiceholder } from "plaiceholder"
 
-export const getItems = async (query: string) => {
-  // https://rapidapi.com/guides/query-parameters-fetch
-  const params = new URLSearchParams({
-    query: query,
-    display: '10'
-  }) //encoded UTF-8
-
-  const res = await fetch(`https://openapi.naver.com/v1/search/shop?${params.toString()}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-NAVER-CLIENT-ID': `${process.env.NAVER_CLIENT_ID}`,
-      'X-NAVER-CLIENT-SECRET': `${process.env.NAVER_CLIENT_SECRET}`,
-    },
-  })
-  if (res.status === 200) {
-    const data = await res.json()
-    const filteredData = data.items.filter((item: Item) => item.category2 === '건강식품')
-    return filteredData
-  }
-}
-
-export async function getBase64(imgUrl: string) {
-  let base64 =
-    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8++TddwAI/QOoDfU+RQAAAABJRU5ErkJggg=='
-  try {
-    const res = await fetch(imgUrl)
-    if (!res.ok) {
-      console.error('Failed to fetch image for base64')
-      return base64
-    }
-    const buffer = await res.arrayBuffer()
-    const result = await getPlaiceholder(Buffer.from(buffer), { size: 10 })
-    base64 = result.base64
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
-  return base64
-}
 
 export default async function Page() {
-
+  
+  const getItems = async (query: string) => {
+    // https://rapidapi.com/guides/query-parameters-fetch
+    const params = new URLSearchParams({
+      query: query,
+      display: '10'
+    }) //encoded UTF-8
+  
+    const res = await fetch(`https://openapi.naver.com/v1/search/shop?${params.toString()}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-NAVER-CLIENT-ID': `${process.env.NAVER_CLIENT_ID}`,
+        'X-NAVER-CLIENT-SECRET': `${process.env.NAVER_CLIENT_SECRET}`,
+      },
+    })
+    if (res.status === 200) {
+      const data = await res.json()
+      const filteredData = data.items.filter((item: Item) => item.category2 === '건강식품')
+      return filteredData
+    }
+  }
+  
+  const getBase64 = async (imgUrl: string)=> {
+    let base64 =
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8++TddwAI/QOoDfU+RQAAAABJRU5ErkJggg=='
+    try {
+      const res = await fetch(imgUrl)
+      if (!res.ok) {
+        console.error('Failed to fetch image for base64')
+        return base64
+      }
+      const buffer = await res.arrayBuffer()
+      const result = await getPlaiceholder(Buffer.from(buffer), { size: 10 })
+      base64 = result.base64
+    } catch (error) {
+      console.error(error)
+      throw error
+    }
+    return base64
+  }
   const items = await getItems('비타민') as Item[]
   const parsed = await Promise.all(items.map(async (item) => {
     const base64 = await getBase64(item.image)
